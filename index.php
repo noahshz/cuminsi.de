ef5ce346bd25de55ab4930f48e69d07876c00e98
Noah <90244021+noahshz@users.noreply.github.com>
2022-02-03 16:14:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="column left">
+
+        </div>
+        <div class="column center">
+            <h1>Cuminsi.de</h1>
+        </div>
+        <div class="column right">
+
+        </div>
+    </div>
+    <div class="content">
+        <p>sad</p>
+    </div>
+    <div class="footer">
+        <p></p>
+    </div>
+</body>
+</html>
