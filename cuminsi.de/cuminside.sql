24473736d026d485e4008f7e20375dc8419085cc
Noah <n.scholziii@gmail.com>
2022-02-25 10:52:26 +0100

    updatet db structure

--- cuminsi.de/cuminside.sql
+++ cuminsi.de/cuminside.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Erstellungszeit: 11. Feb 2022 um 22:51
--- Server-Version: 10.3.31-MariaDB-0+deb10u1
--- PHP-Version: 7.3.31-1~deb10u1
+-- Host: 127.0.0.1
+-- Erstellungszeit: 25. Feb 2022 um 10:51
+-- Server-Version: 10.4.21-MariaDB
+-- PHP-Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -22,6 +22,20 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `cuminside` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `cuminside`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `uid` int(255) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `link` text NOT NULL,
+  `imgpath` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -43,6 +57,12 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indizes für die Tabelle `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indizes für die Tabelle `users`
 --
 ALTER TABLE `users`
@@ -53,6 +73,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT für exportierte Tabellen
 --
+
+--
+-- AUTO_INCREMENT für Tabelle `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
