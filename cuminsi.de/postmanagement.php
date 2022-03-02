240eee54f788e23a7378c017a0676b8cb9e7bec4
Noah <n.scholziii@gmail.com>
2022-03-02 14:14:55 +0100

    Update postmanagement.php

--- cuminsi.de/postmanagement.php
+++ cuminsi.de/postmanagement.php
@@ -3,7 +3,6 @@
 
     $session = new Session();
     if(!$session->isset()){header('Location: index.php?error_code=9050');}
-    if($session->get('verified') == "false"){header('Location: index.php?error_code=9051');}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,28 +23,46 @@
     <hr>
 
     <?php
+        displayMessageOrError();
+
         if(isset($_GET['tab'])) {
             switch($_GET['tab']) {
                 case 'uploaded':
-                    //show tab with own uploaded posts
-                    include 'template_uploadedPosts.php';
+                    //show tab with own uploaded posts - allow only verified
+                    if($session->get('verified') == "false") {
+                        echo "to use this function pleaser verify first";
+                    } else {
+                        include 'template_uploadedPosts.php';
+                    }
                     break;
                 case 'liked':
-                    //show tab with liked posts
+                    //show tab with liked posts - allow for everyone
                     include 'template_likedPosts.php';
                     break;
                 case 'create':
-                    //show tab to create post
-                    include 'template_createPost.php';
+                    //show tab to create post - allow only verified
+                    if($session->get('verified') == "false") {
+                        echo "to use this function pleaser verify first";
+                    } else {
+                        include 'template_createPost.php';
+                    }
                     break;
                 default:
                     //default case: show uploaded posts
-                    include 'template_uploadedPosts.php';
+                    if($session->get('verified') == "false") {
+                        include 'template_likedPosts.php';
+                    } else {
+                        include 'template_uploadedPosts.php';
+                    }
                     break;
             }
         } else {
             //if site is opened without get variable then own posts are displayed
-            include 'template_uploadedPosts.php';
+            if($session->get('verified') == "false") {
+                include 'template_likedPosts.php';
+            } else {
+                include 'template_uploadedPosts.php';
+            }
         }
     ?>
 </body>
