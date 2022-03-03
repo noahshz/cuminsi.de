51ccd58ba2800e4c64cd8f07096323c143523f9e
Noah <n.scholziii@gmail.com>
2022-03-03 14:32:37 +0100

    Update requirements.php

--- cuminsi.de/requirements.php
+++ cuminsi.de/requirements.php
@@ -3,4 +3,7 @@
     require 'db.php'; //DB-Connection
     require 'functions.php'; //Global Functions
     require 'classes.php'; //Global Classes
+
+    
+    //output_add_rewrite_var('cmisitoken', 'XPIc8lFiHWDpQ38jmv3wKXWFG5umF3Tb32rTnrwBL7Bd7JKIVBAiCcO1s6cK');
 ?>
